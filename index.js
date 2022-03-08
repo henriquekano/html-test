427780a3f611081fa3ab7b1aeeac7d06c721ee8f
henriquekano <henriquekano@hotmail.com>
2022-03-08 17:55:54 -0300

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var observer = new MutationObserver(console.log);
+var elemDiv = document.createElement("div");
+elemDiv.innerText = "Working I am";
+document.body.appendChild(elemDiv);
 
+var observer = new MutationObserver(console.log);
+console.log(document.body);
 observer.observe(document.body, {
   subtree: true,
   childList: true,
